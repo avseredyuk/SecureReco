e36b38beb66fe468f9e0a1a35b061de53b43d115
xurset <a.v.seredyuk@gmail.com>
2017-06-01 06:34:10 +0300

    on the way to fully implement #8

--- app/src/main/java/com/avseredyuk/securereco/activity/MainActivity.java
+++ app/src/main/java/com/avseredyuk/securereco/activity/MainActivity.java
@@ -1,31 +1,45 @@
 package com.avseredyuk.securereco.activity;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.avseredyuk.securereco.R;
-import com.avseredyuk.securereco.adapter.CallArrayAdapter;
 import com.avseredyuk.securereco.application.Application;
 import com.avseredyuk.securereco.auth.AuthenticationManager;
 import com.avseredyuk.securereco.dao.CallDao;
 import com.avseredyuk.securereco.exception.AuthenticationException;
 import com.avseredyuk.securereco.model.Call;
 import com.avseredyuk.securereco.util.ConfigUtil;
+import com.avseredyuk.securereco.util.ContactResolverUtil;
+import com.avseredyuk.securereco.util.StringUtil;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -34,9 +48,13 @@ import java.util.List;
 import static com.avseredyuk.securereco.util.Constant.IS_ENABLED;
 import static com.avseredyuk.securereco.util.Constant.NOTIFICATION_ON;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl {
     private CallArrayAdapter callArrayAdapter;
-    List<Call> calls = new ArrayList<>();
+    private List<Call> calls = new ArrayList<>();
+    private MediaPlayer mediaPlayer;
+    private MediaController mediaController;
+    private Handler handler = new Handler();
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -94,6 +112,15 @@ public class MainActivity extends AppCompatActivity {
         if (!Application.getInstance().authHolder.isLocked()) {
             Application.getInstance().eraseAuthMan();
         }
+
+        if (mediaController != null) {
+            mediaController.hide();
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
         System.out.println("MA STOPPED");
     }
 
@@ -261,6 +288,196 @@ public class MainActivity extends AppCompatActivity {
             toastText = getString(R.string.toast_nothing_to_delete);
         }
         Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    private static class ViewHolder {
+        TextView firstLine;
+        TextView secondLine;
+        TextView thirdLine;
+        ImageView imageView;
+        ImageButton playBtn;
+        CheckBox checkBox;
+    }
+
+    class CallArrayAdapter extends ArrayAdapter<Call>
+            implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+        private final List<Integer> checkedItemsIndexes = new ArrayList<>();
+
+
+
+        public CallArrayAdapter(Context context, List<Call> calls) {
+            super(context, R.layout.list_item, calls);
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            Call call = this.getItem(position);
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+                viewHolder.firstLine = (TextView) convertView.findViewById(R.id.contactName);
+                viewHolder.secondLine = (TextView) convertView.findViewById(R.id.callNumber);
+                viewHolder.thirdLine = (TextView) convertView.findViewById(R.id.callDate);
+                viewHolder.imageView = (ImageView) convertView.findViewById(R.id.avatar);
+                viewHolder.playBtn = (ImageButton) convertView.findViewById(R.id.playButton);
+                viewHolder.checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
+
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            viewHolder.firstLine.setText(ContactResolverUtil.getContactName(getContext(), call.getCallNumber()));
+            viewHolder.firstLine.setTextColor(
+                    call.isIncoming()
+                            ? getContext().getResources().getColor(R.color.colorCallIncoming)
+                            : getContext().getResources().getColor(R.color.colorCallOutgoing));
+            viewHolder.secondLine.setText(call.getCallNumber());
+            viewHolder.thirdLine.setText(StringUtil.formatDate(call.getDatetimeStarted()));
+            viewHolder.imageView.setImageBitmap(ContactResolverUtil.retrieveContactPhoto(getContext(), call.getCallNumber()));
+            viewHolder.playBtn.setTag(call);
+            viewHolder.playBtn.setOnClickListener(this);
+            viewHolder.checkBox.setTag(position);
+            viewHolder.checkBox.setOnCheckedChangeListener(this);
+            boolean isItemChecked = checkedItemsIndexes.contains(position);
+            viewHolder.checkBox.setChecked(isItemChecked);
+
+            return convertView;
+        }
+
+        public List<Integer> getCheckedStatuses() {
+            return new ArrayList<>(checkedItemsIndexes);
+        }
+
+        public int getCheckedCount() {
+            return checkedItemsIndexes.size();
+        }
+
+        public void resetCheckedItems() {
+            checkedItemsIndexes.clear();
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            Integer position = (Integer) buttonView.getTag();
+            if (isChecked) {
+                checkedItemsIndexes.add(position);
+            } else {
+                checkedItemsIndexes.remove(position);
+            }
+        }
+
+        @Override
+        public void onClick(View v) {
+            Call call = (Call) v.getTag();
+
+            if (Application.getInstance().isAuthenticated()) {
+                byte[] callData = CallDao.getInstance().play(call, Application.getInstance().getAuthMan());
+
+                String base64EncodedString = Base64.encodeToString(callData, Base64.DEFAULT);
+                mediaController = new MediaController(MainActivity.this);
+                try {
+                    String url = "data:audio/amr;base64,"+base64EncodedString;
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(url);
+                    mediaPlayer.setOnPreparedListener(MainActivity.this);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                } catch(Exception ex){
+                    System.out.print(ex.getMessage());
+                }
+
+//            Intent playIntent = new Intent(Application.getInstance(), PlayerActivity.class);
+//            playIntent.putExtra(CALL_DECRYPTED_INTENT_DATA_NAME, callData);
+//            getContext().startActivity(playIntent);
+            } else {
+                Toast.makeText(getContext(),
+                        getContext().getString(R.string.toast_please_authenticate_first),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> ON TOUCH EVENT");
+        //the MediaController will hide after 3 seconds - tap the screen to make it appear again
+        mediaController.show();
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.d("tag", "onPrepared");
+        System.out.println("__________________________________________ PREPARED");
+        mediaController.setMediaPlayer(this);
+        mediaController.setAnchorView(findViewById(R.id.main_activity));
+
+        handler.post(new Runnable() {
+            public void run() {
+                mediaController.setEnabled(true);
+                mediaController.show();
+            }
+        });
+    }
+
+    @Override
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 
 }
