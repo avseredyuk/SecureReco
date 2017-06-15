9cd52d655684bc4747e815eb59181f650c738a63
Anton_Serediuk <a.v.seredyuk@gmail.com>
2017-06-15 13:43:13 +0300

    made Call class Parcelable

--- app/src/main/java/com/avseredyuk/securereco/model/Call.java
+++ app/src/main/java/com/avseredyuk/securereco/model/Call.java
@@ -1,12 +1,15 @@
 package com.avseredyuk.securereco.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Comparator;
 import java.util.Date;
 
 /**
  * Created by lenfer on 2/15/17.
  */
-public class Call {
+public class Call implements Parcelable {
     private String callNumber;
     private Date datetimeStarted;
     private Date dateTimeEnded;
@@ -66,4 +69,41 @@ public class Call {
     public void setDateTimeEnded(Date dateTimeEnded) {
         this.dateTimeEnded = dateTimeEnded;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    protected Call(Parcel in) {
+        callNumber = in.readString();
+        long tmpDatetimeStarted = in.readLong();
+        datetimeStarted = tmpDatetimeStarted != -1 ? new Date(tmpDatetimeStarted) : null;
+        long tmpDateTimeEnded = in.readLong();
+        dateTimeEnded = tmpDateTimeEnded != -1 ? new Date(tmpDateTimeEnded) : null;
+        isIncoming = in.readByte() != 0x00;
+        filename = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(callNumber);
+        dest.writeLong(datetimeStarted != null ? datetimeStarted.getTime() : -1L);
+        dest.writeLong(dateTimeEnded != null ? dateTimeEnded.getTime() : -1L);
+        dest.writeByte((byte) (isIncoming ? 0x01 : 0x00));
+        dest.writeString(filename);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Call> CREATOR = new Parcelable.Creator<Call>() {
+        @Override
+        public Call createFromParcel(Parcel in) {
+            return new Call(in);
+        }
+
+        @Override
+        public Call[] newArray(int size) {
+            return new Call[size];
+        }
+    };
 }
