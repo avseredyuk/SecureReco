avseredyuk/SecureReco

commit 5b402c1e055c010163f660dd16270031032d591f

fixed #36
* introduced LoadCallsTask to load calls from storage asynchronously
* fixed MainActivity.onResume() to handle calls list in the extra section of the intent

app/src/main/java/com/avseredyuk/securereco/activity/MainActivity.java
@@ -39,6 +39,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static com.avseredyuk.securereco.util.Constant.CALLS_LIST_PARCEL_NAME;
 import static com.avseredyuk.securereco.util.Constant.IS_ENABLED;
 import static com.avseredyuk.securereco.util.Constant.NOTIFICATION_ON;
 
@@ -69,8 +70,16 @@ public class MainActivity extends SecuredActivity
     @Override
     protected void onResume() {
         super.onResume();
-        calls.clear();
-        calls.addAll(CallDao.getInstance().findAll(Call.CallDateComparator));
+
+        Intent intent = getIntent();
+        List<Call> callsListFromIntent = intent.getParcelableArrayListExtra(CALLS_LIST_PARCEL_NAME);
+        if (callsListFromIntent != null) {
+            calls.clear();
+            calls.addAll(callsListFromIntent);
+        } else {
+            calls.clear();
+            calls.addAll(CallDao.getInstance().findAll(Call.CallDateComparator));
+        }
         callArrayAdapter.notifyDataSetChanged();
     }
 

app/src/main/java/com/avseredyuk/securereco/activity/SplashActivity.java
@@ -1,18 +1,20 @@
 package com.avseredyuk.securereco.activity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 
 import com.avseredyuk.securereco.application.Application;
+import com.avseredyuk.securereco.dao.CallDao;
+import com.avseredyuk.securereco.model.Call;
 import com.avseredyuk.securereco.service.RecorderService;
 import com.avseredyuk.securereco.util.ConfigUtil;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.avseredyuk.securereco.util.Constant.SPLASH_SHOW_TIME_IN_SECONDS;
+import static com.avseredyuk.securereco.util.Constant.CALLS_LIST_PARCEL_NAME;
 
 /**
  * Created by lenfer on 3/1/17.
@@ -24,16 +26,28 @@ public class SplashActivity extends AppCompatActivity {
 
         startService(new Intent(Application.getInstance(), RecorderService.class));
 
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(SPLASH_SHOW_TIME_IN_SECONDS));
-
-        final Class<? extends Activity> activityClass;
         if (ConfigUtil.isConfigValid()) {
-            activityClass = MainActivity.class;
+            LoadCallsTask loadTask = new LoadCallsTask();
+            loadTask.execute();
         } else {
-            activityClass = FirstRunActivity.class;
+            startActivity(new Intent(Application.getInstance(), FirstRunActivity.class));
+            finish();
         }
-        startActivity(new Intent(Application.getInstance(), activityClass));
+    }
 
-        finish();
+    private class LoadCallsTask extends AsyncTask<Void, Void, List<Call>> {
+        @Override
+        protected List<Call> doInBackground(Void... voids) {
+            return CallDao.getInstance().findAll(Call.CallDateComparator);
+        }
+
+        @Override
+        protected void onPostExecute(List<Call> calls) {
+            super.onPostExecute(calls);
+            startActivity(new Intent(Application.getInstance(), MainActivity.class)
+                    .putParcelableArrayListExtra(CALLS_LIST_PARCEL_NAME,
+                            (ArrayList<Call>) CallDao.getInstance().findAll(Call.CallDateComparator)));
+            finish();
+        }
     }
 }

app/src/main/java/com/avseredyuk/securereco/util/Constant.java
@@ -24,5 +24,6 @@ public class Constant {
     public static final String OLD_FOLDER_PATH = "oldFolderPath";
     public static final String NEW_FOLDER_PATH = "newFolderPath";
     public static final String INTENT_BROADCAST_RESET_AUTH = "com.avseredyuk.securereco.RESET_AUTH";
+    public static final String CALLS_LIST_PARCEL_NAME = "callsListParcel";
     public static final int RESET_AUTH_DELAY = 1000 * 60;
 }

commit 5fdfddb30202bf59264a7262c50d8484e0e62a83

fixed broken calls list update that caused by 5b402c1e055c010163f660dd16270031032d591f

app/src/main/java/com/avseredyuk/securereco/activity/MainActivity.java
@@ -76,6 +76,7 @@ public class MainActivity extends SecuredActivity
         if (callsListFromIntent != null) {
             calls.clear();
             calls.addAll(callsListFromIntent);
+            intent.putExtra(CALLS_LIST_PARCEL_NAME, (String) null);
         } else {
             calls.clear();
             calls.addAll(CallDao.getInstance().findAll(Call.CallDateComparator));