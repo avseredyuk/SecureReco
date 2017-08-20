2fd33220ca4c4f07d837cf8e254ecf556c19ac37
avseredyuk <a.v.seredyuk@gmail.com>
2017-08-20 18:02:39 +0300

    minor refactoring of ApplicationNotification

--- app/src/main/java/com/avseredyuk/securereco/notification/ApplicationNotification.java
+++ app/src/main/java/com/avseredyuk/securereco/notification/ApplicationNotification.java
@@ -17,11 +17,10 @@ import static com.avseredyuk.securereco.util.Constant.NOTIFICATION_ID;
  */
 
 public abstract class ApplicationNotification {
-    protected final NotificationColor notificationColor =
+    final NotificationColor notificationColor =
             Application.getInstance().getConfiguration().getNotificationColor();
     final Context context;
     protected final Call call;
-    private RemoteViews contentView;
 
     int button1;
     int button2;
@@ -29,7 +28,6 @@ public abstract class ApplicationNotification {
     String notificationText2;
     PendingIntent button1PendingIntent;
     PendingIntent button2PendingIntent;
-
 
     ApplicationNotification(Context context, Call call) {
         this.context = context;
@@ -39,9 +37,32 @@ public abstract class ApplicationNotification {
     public void alert() {
         if (Application.getInstance().getConfiguration().isNotificationOn()) {
 
-            initializeLayout();
+            RemoteViews contentView = new RemoteViews(context.getPackageName(),
+                    R.layout.notification_layout);
+
+            contentView.setInt(R.id.notificationLayout, "setBackgroundResource",
+                    notificationColor == NotificationColor.DAY
+                            ? R.color.notificationBgColor
+                            : R.color.notificationTextColor);
+
+            contentView.setTextColor(R.id.notification_text_1, context.getResources().getColor(
+                    notificationColor == NotificationColor.DAY
+                            ? R.color.notificationTextColor
+                            : R.color.notificationBgColor));
+
+            contentView.setTextColor(R.id.notification_text_2, context.getResources().getColor(
+                    notificationColor == NotificationColor.DAY
+                            ? R.color.notificationTextColor
+                            : R.color.notificationBgColor));
 
             contentView.setImageViewBitmap(R.id.notification_contact_photo, call.getPhoto());
+
+            contentView.setImageViewResource(R.id.notification_button_1, button1);
+            contentView.setImageViewResource(R.id.notification_button_2, button2);
+            contentView.setTextViewText(R.id.notification_text_1, notificationText1);
+            contentView.setTextViewText(R.id.notification_text_2, notificationText2);
+            contentView.setOnClickPendingIntent(R.id.notification_button_1, button1PendingIntent);
+            contentView.setOnClickPendingIntent(R.id.notification_button_2, button2PendingIntent);
 
             android.app.Notification notification = new android.app.Notification.Builder(context)
                     .setSmallIcon(R.drawable.button_play)
@@ -52,30 +73,5 @@ public abstract class ApplicationNotification {
             ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
                     .notify(NOTIFICATION_ID, notification);
         }
-    }
-
-    void initializeLayout() {
-        // Common init
-        contentView = new RemoteViews(context.getPackageName(),
-                R.layout.notification_layout);
-        contentView.setInt(R.id.notificationLayout, "setBackgroundResource",
-                notificationColor == NotificationColor.DAY
-                        ? R.color.notificationBgColor
-                        : R.color.notificationTextColor);
-        contentView.setTextColor(R.id.notification_text_1, context.getResources().getColor(
-                notificationColor == NotificationColor.DAY
-                        ? R.color.notificationTextColor
-                        : R.color.notificationBgColor));
-        contentView.setTextColor(R.id.notification_text_2, context.getResources().getColor(
-                notificationColor == NotificationColor.DAY
-                        ? R.color.notificationTextColor
-                        : R.color.notificationBgColor));
-        // Individual init
-        contentView.setImageViewResource(R.id.notification_button_1, button1);
-        contentView.setImageViewResource(R.id.notification_button_2, button2);
-        contentView.setTextViewText(R.id.notification_text_1, notificationText1);
-        contentView.setTextViewText(R.id.notification_text_2, notificationText2);
-        contentView.setOnClickPendingIntent(R.id.notification_button_1, button1PendingIntent);
-        contentView.setOnClickPendingIntent(R.id.notification_button_2, button2PendingIntent);
     }
 }
